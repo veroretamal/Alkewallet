ace00d8806321d5b036c15f2a5fca4a550450a0a
veroretamal <veronicaretamalarriagada@gmail.com>
2024-02-29 12:21:34 -0300

    Primer commit del proyecto

---
+++ java/deposit.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('#botonDeposito').click(function() {
+      // Obtener el valor del monto a depositar
+      const monto = $('#monto').val();
+  
+      // Validar el monto (opcional)
+  
+      // Enviar el monto a la API o método de depósito
+      // ...
+  
+      // Mostrar un mensaje de éxito o error
+      // ...
+    });
+  });
+  
